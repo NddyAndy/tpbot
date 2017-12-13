19691c586e55f344594e95a85beab5a521691c55
Nddy Andy <iam.ndie@gmail.com>
2017-12-13 14:59:51 +0100

    heroku stuff

--- app.js
+++ app.js
@@ -132,15 +132,15 @@ function handleMessage(sender_psid, received_message) {
          let webhook_event = entry.messaging[0];
          console.log(webhook_event);
          
-         let sender_psid = webhook_event.sender.id;
-         console.log('Sender PSID: ' + sender_psid);
-         // Check if the event is a message or postback and
-        // pass the event to the appropriate handler function
-         if (webhook_event.message) {
-          handleMessage(sender_psid, webhook_event.message);        
-        } else if (webhook_event.postback) {
-          handlePostback(sender_psid, webhook_event.postback);
-        }
+        //  let sender_psid = webhook_event.sender.id;
+        //  console.log('Sender PSID: ' + sender_psid);
+        //  // Check if the event is a message or postback and
+        // // pass the event to the appropriate handler function
+        //  if (webhook_event.message) {
+        //   handleMessage(sender_psid, webhook_event.message);        
+        // } else if (webhook_event.postback) {
+        //   handlePostback(sender_psid, webhook_event.postback);
+        // }
   
        });
    
