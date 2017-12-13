ad6814238ca3ba01e85763e0fef8ff674035a31b
Nddy Andy <iam.ndie@gmail.com>
2017-12-13 15:45:29 +0100

    heroku ish1

--- app.js
+++ app.js
@@ -9,109 +9,7 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-  // Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-  let response;
-    // Check if the message contains text
-    if (received_message.text) {
-
-      // add variable here
-      let chobaToAlakahia = /from choba to alakahia/;
-
   
-      if(received_message.text.match(chobaToAlakahia)) {
-        response = {
-          "text": `Take a taxi at "choba junction" going to alakahia!\ncost: N50. \nHave a safe trip!`
-        }
-      }
-      // else if () {
-
-      // } 
-      else {
-        response = {
-          "text": `You sent the message: "${received_message.text}". Now send me a direction like so: \n"From dline to okoro"!`
-        }
-      }
-      // Create the payload for a basic text message
-      
-    } else if (received_message.attachments) {
-      // Get the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "generic",
-            "elements": [{
-              "title": "Is this the right picture?",
-              "subtitle": "Tap a button to answer.",
-              "image_url": attachment_url,
-              "buttons": [
-                {
-                  "type": "postback",
-                  "title": "Yes!",
-                  "payload": "yes",
-                },
-                {
-                  "type": "postback",
-                  "title": "No!",
-                  "payload": "no",
-                }
-              ],
-            }]
-          }
-        }
-      }  
-    }
-    // Sends the response message
-    callSendAPI(sender_psid, response);   
-  }
-  
-  // Handles messaging_postbacks events
-  function handlePostback(sender_psid, received_postback) {
-    let response;
-    
-    // Get the payload for the postback
-    let payload = received_postback.payload;
-  
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-      response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-      response = { "text": "Oops, try sending another image." }
-    }
-    // Send the message to acknowledge the postback
-    callSendAPI(sender_psid, response);
-  }
-  
-  // Sends response messages via the Send API
-  function callSendAPI(sender_psid, response) {
-      // Construct the message body
-      let request_body = {
-        "recipient": {
-          "id": sender_psid
-        },
-        "message": response
-      }
-
-        // Send the HTTP request to the Messenger Platform
-        request({
-          "uri": "https://graph.facebook.com/v2.6/me/messages",
-          "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-          "method": "POST",
-          "json": request_body
-        }, (err, res, body) => {
-          if (!err) {
-            console.log('message sent!')
-          } else {
-            console.error("Unable to send message:" + err);
-          }
-        }); 
-  }
-
   app.get('/', (req, res) => {
     res.status(200).json({msg: 'hello world'});
     console.log(process.env.PAGE_ACCESS_TOKEN + " " + typeof process.env.PAGE_ACCESS_TOKEN);
@@ -133,15 +31,6 @@ function handleMessage(sender_psid, received_message) {
          let webhook_event = entry.messaging[0];
          console.log(webhook_event);
          
-         let sender_psid = webhook_event.sender.id;
-         console.log('Sender PSID: ' + sender_psid);
-         // Check if the event is a message or postback and
-        // pass the event to the appropriate handler function
-         if (webhook_event.message) {
-          handleMessage(sender_psid, webhook_event.message);        
-        } else if (webhook_event.postback) {
-          handlePostback(sender_psid, webhook_event.postback);
-        }
   
        });
    
